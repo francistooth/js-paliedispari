8df0642c80c5d6ee77c5aa86d95156e8b314d692
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-12 18:20:56 +0200

    palindrome 1.0.2: fix: paragraph implemented

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,5 +1,3 @@
-let in1 = prompt("dammi una parola, verifichero' che sia palindroma")
-
 function isPalindrome(str) {
    let j = str.length - 1;
    for (let i = 0; i < str.length / 2; i++) {
@@ -11,6 +9,16 @@ function isPalindrome(str) {
    return true;
 }
 
+let in1 = prompt("dammi una parola, verifichero' che sia palindroma")
 
 console.log(in1);
 console.log(isPalindrome(in1));
+
+const element = document.getElementById("text");
+
+const p1 = document.createElement("p");
+
+const node = document.createTextNode(in1 + "  " + isPalindrome(in1));
+
+p1.appendChild(node);
+element.appendChild(p1);
